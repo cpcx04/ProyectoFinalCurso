ed5165b6471cd1ff804310e19db1b5e5591e521c
Cristian Pulido <pulidocabellochristian@gmail.com>
2023-04-21 12:30:31 +0200

    "Casi ready la pagina estatica"

---
+++ html/scrip/proyecto.js
@@ -0,0 +1,9 @@
+
+let flecha = document.getElementById("cerrarMenu").addEventListener("click",function(){
+        let menu = document.getElementById("menu");
+        if (menu.hidden) {
+            menu.hidden = false;
+        } else
+            menu.hidden = true;
+})
+
