7ab420b13b57a5dcb2ca6e2e5aa2ae5fd61e3ff9
Cristian Pulido <pulidocabellochristian@gmail.com>
2023-05-02 09:54:47 +0200

    Primera prueba con Spring

---
+++ spring/ProyectoFinalCurso/src/test/java/com/salesianostriana/dam/composicion/proyectofinalcurso/ProyectoFinalCursoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salesianostriana.dam.composicion.proyectofinalcurso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoFinalCursoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
