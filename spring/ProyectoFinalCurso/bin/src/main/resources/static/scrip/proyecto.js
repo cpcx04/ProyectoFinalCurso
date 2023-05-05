7e52df0f4b80c999556efa40b12525558957c420
Cristian Pulido <pulidocabellochristian@gmail.com>
2023-05-05 08:28:59 +0200

    "leves"

---
+++ spring/ProyectoFinalCurso/bin/src/main/resources/static/scrip/proyecto.js
@@ -0,0 +1,15 @@
+let botonOcultar = document.getElementById('cerrarMenu');
+botonOcultar.addEventListener('click', ocultarImg);
+
+function ocultarImg() {
+    let div = document.querySelector('div');
+    if (div.hidden) {
+        div.hidden = false;
+    } else
+        div.hidden = true;
+}
+
+var myObject = document.querySelector('svg');
+myObject.addEventListener('click', function() {
+  myObject.classList.toggle('rotate');
+});
