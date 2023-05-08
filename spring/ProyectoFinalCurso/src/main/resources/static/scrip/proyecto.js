942fee1438809ecb633008220068dcc7813d9465
cpcx04 <pulidocabellochristian@gmail.com>
2023-05-08 17:00:42 +0200

    ""

--- spring/ProyectoFinalCurso/src/main/resources/static/scrip/proyecto.js
+++ spring/ProyectoFinalCurso/src/main/resources/static/scrip/proyecto.js
@@ -13,3 +13,10 @@ var myObject = document.querySelector('svg');
 myObject.addEventListener('click', function () {
   myObject.classList.toggle('rotate');
 });
+/*Cargado de Pagina*/
+//funcion para quitar el preoloader
+function quitarPreloader() {
+  const preloader = document.querySelector('.onload');
+  preloader.style.opacity = 0;
+  preloader.style.visibility = 'hidden';
+}
