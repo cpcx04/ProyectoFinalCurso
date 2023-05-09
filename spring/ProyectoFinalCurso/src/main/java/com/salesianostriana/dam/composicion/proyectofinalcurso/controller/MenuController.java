83161aeaf97fe3bd57ff626695a03479f92575f1
cpcx04 <pulidocabellochristian@gmail.com>
2023-05-09 17:27:47 +0200

    "insercion de datos en la bdd"

--- spring/ProyectoFinalCurso/src/main/java/com/salesianostriana/dam/composicion/proyectofinalcurso/controller/MenuController.java
+++ spring/ProyectoFinalCurso/src/main/java/com/salesianostriana/dam/composicion/proyectofinalcurso/controller/MenuController.java
@@ -1,8 +1,22 @@
 package com.salesianostriana.dam.composicion.proyectofinalcurso.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.salesianostriana.dam.composicion.proyectofinalcurso.service.MenuService;
 
 @Controller
+@RequestMapping("/menu")
 public class MenuController {
-
+	@Autowired
+	private MenuService menuService;
+	
+	@GetMapping("/")
+	public String index(Model model) {
+		model.addAttribute("comidas",menuService.findAll());
+		return "/menu";
+	}
 }
--- spring/ProyectoFinalCurso/src/main/java/com/salesianostriana/dam/composicion/proyectofinalcurso/service/MenuService.java
+++ spring/ProyectoFinalCurso/src/main/java/com/salesianostriana/dam/composicion/proyectofinalcurso/service/MenuService.java
@@ -1,5 +1,7 @@
 package com.salesianostriana.dam.composicion.proyectofinalcurso.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,5 +14,25 @@ public class MenuService{
 	@Autowired
 	private MenuRepository comidas;
 	
+	public List<Menu> findAll(){
+		return comidas.findAll();
+	}
 	
+	public List<Menu> findComidas(){
+		return comidas.findComidas();
+	}
+	
+	public Menu save(Menu comida) {
+		return comidas.save(comida);
+	}
+	
+	public Menu findById(Long id) {
+		return comidas.findById(id).orElse(null);
+	}
+	
+	public Menu delete(Menu comida) {
+		Menu result= findById(comida.getId_dieta());
+		comidas.delete(result);
+		return result;
+	}
 }
