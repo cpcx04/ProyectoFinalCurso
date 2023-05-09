9c002570e1868c9ee57b9dee17530228ff27cc9e
Cristian Pulido <pulidocabellochristian@gmail.com>
2023-05-09 12:44:34 +0200

    "preloader completo"

--- spring/ProyectoFinalCurso/src/main/resources/static/scrip/proyecto.js
+++ spring/ProyectoFinalCurso/src/main/resources/static/scrip/proyecto.js
@@ -11,7 +11,7 @@ let botonOcultar = document.getElementById('cerrarMenu');
 botonOcultar.addEventListener('click', ocultarImg);
 
 function ocultarImg() {
-  let div = document.querySelector('div');
+  let div = document.querySelector('nav');
   if (div.hidden) {
     div.hidden = false;
   } else
