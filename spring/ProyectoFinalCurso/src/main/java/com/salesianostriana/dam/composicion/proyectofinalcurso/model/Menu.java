46c0fc37592e0a7d212d3d827b12aba9aca38ab4
Cristian Pulido <pulidocabellochristian@gmail.com>
2023-05-10 10:57:52 +0200

    "Cambios en CrudMENU"

--- spring/ProyectoFinalCurso/src/main/java/com/salesianostriana/dam/composicion/proyectofinalcurso/model/Menu.java
+++ spring/ProyectoFinalCurso/src/main/java/com/salesianostriana/dam/composicion/proyectofinalcurso/model/Menu.java
@@ -1,9 +1,11 @@
 package com.salesianostriana.dam.composicion.proyectofinalcurso.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -16,8 +18,9 @@ import lombok.NoArgsConstructor;
 @Entity
 public class Menu {
 	
+
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	 @GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id_dieta;
 	
 	private String platoPrincipal;
