e6bd77ae8a677e0763a30e50aff02acd8883658d
Cristian Pulido <pulidocabellochristian@gmail.com>
2023-05-15 10:58:38 +0200

    "formEditarEmpleado"

--- spring/ProyectoFinalCurso/src/main/java/com/salesianostriana/dam/composicion/proyectofinalcurso/model/Profesor.java
+++ spring/ProyectoFinalCurso/src/main/java/com/salesianostriana/dam/composicion/proyectofinalcurso/model/Profesor.java
@@ -4,11 +4,9 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 import lombok.Data;
-import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 @Data
-@EqualsAndHashCode(callSuper=false)
 @Entity
 @NoArgsConstructor
 @Table(name="PROFESOR")
